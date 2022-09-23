a42fcdab9acdd398e4776f0b407f6b6f243ffb1e
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-23 19:47:38 +0530

    Added :Created invoice generator which returned the total fare for the journey

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Cab Invoice Generator");
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+
+    public static double calculateFare(double distance, double time) {
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        return Math.max(totalFare, MINIMUM_FARE);
     }
 }
--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -1,10 +1,22 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CabInvoiceTest {
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        InvoiceGenerator generator = new InvoiceGenerator();
+        double distance = 2.0;
+        double time = 5.0;
+        double fare = InvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+        double distance = 0.1;
+        double time = 1.0;
+        double fare = InvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
