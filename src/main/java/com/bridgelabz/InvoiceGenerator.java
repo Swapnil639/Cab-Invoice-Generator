4a5302ee334cfd0d4da19bd67605a0db116e603d
Swapnil639 <swapnilpatil631997@gmail.com>
2022-09-24 19:24:22 +0530

    Added :Find out total number of rides,total fare and average fare per ride

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -2,20 +2,20 @@ package com.bridgelabz;
 
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
-    private static final double COST_PER_TIME = 1;
+    private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_FARE = 5;
 
-    public static double calculateFare(double distance,double time) {
+
+    public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
 
-    public double calculateFare(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.distance, ride.time);
-
         }
-        return totalFare;
+        return new InvoiceSummary(rides.length, totalFare);
     }
 }
